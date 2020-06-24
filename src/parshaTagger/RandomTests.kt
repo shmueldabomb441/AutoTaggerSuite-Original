be70f82ba54fb3453514a5e936d99e10d41b8695
Shmuel <shmuelsternbach613@gmail.com>
2020-06-23 22:29:05 -0400

    1. Integrated parsha finding algorithm into real data/actually reading local MP3 files data to identify whether it contains a recognized parsha or not 2. Refactored what is now getParsha() into its own function(with two versions: 1. an overloaded version which doesn't print its findings to the console and accepts a dummy parameter called dontPrintToConsole of type boolean, 2. and one which does print its findings to the console) 3. Introduced a mutable map for rejects(rejected shiur title -> rejection reason) 4. Added function called findMP3sAndAddThemToListOfMp3s which finds all MP3 files in current directory and adds them to a list of files which it is given as a parameter, and returns true if there were no files at all in the given directory

--- src/parshaTagger/RandomTests.kt
+++ src/parshaTagger/RandomTests.kt
@@ -1,6 +1,14 @@
 package parshaTagger
 
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.audio.mp3.MP3File
+import org.jaudiotagger.tag.FieldKey
+import java.io.File
+
 fun main() {
-    for(i in 1..100) for(x in 1..100) for(z in 1..5) println("$i,$x,$z")
-//        if(counter%2==0) {newString.indexOf(char,1,false)='n'
-    }
+    val file = File("C:\\Users\\Shmuel\\Downloads\\good version of JAudioTagger\\JAudioTagger - Copy\\RAL Acharei Mos 5780 The First Rashi; The Nature of Being M'Zarez (From a Q & A on Corona for YGW Girls Division).mp3")
+    val f = AudioFileIO.read(file) as MP3File
+    val tag = f.tag
+    tag.setField(FieldKey.TITLE,"My name is")
+    f.commit()
+}
